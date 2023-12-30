c344503b23d794ab2c9241f774c1033e10344d2d
BakGuno <129590082+BakGuno@users.noreply.github.com>
2023-12-30 21:13:55 +0900

    3주차

---
+++ week3/week3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace week3
+{
+    internal class Program
+    {
+        public class Unit
+        {
+            public void Move()
+            {
+                Console.WriteLine("두발로 걷기");
+            }
+
+            public void Attack()
+            {
+                Console.WriteLine("Unit 공격");
+            }
+        }
+
+        public class Marine : Unit
+        {
+
+        }
+
+        public class Zergling : Unit
+        {
+            public void Move()
+            {
+                Console.WriteLine("네발로 걷기");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Marine marine = new Marine();
+            marine.Move();
+            marine.Attack();
+
+            Zergling zergling = new Zergling();
+            zergling.Move();
+            zergling.Attack();
+        }
+    }
+}
