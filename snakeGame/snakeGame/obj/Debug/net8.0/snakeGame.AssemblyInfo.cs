6699e3deedbdd38890418a7702450988f0ec65af
BakGuno <129590082+BakGuno@users.noreply.github.com>
2024-01-02 12:38:53 +0900

    공부용
    풀이 방법이 생각안나서 공부용으로 올림

--- snakeGame/snakeGame/obj/Debug/net8.0/snakeGame.AssemblyInfo.cs
+++ snakeGame/snakeGame/obj/Debug/net8.0/snakeGame.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("week3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("snakeGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955337021db77332bfb1489f3755b563cf0385c0")]
-[assembly: System.Reflection.AssemblyProductAttribute("week3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("week3")]
+[assembly: System.Reflection.AssemblyProductAttribute("snakeGame")]
+[assembly: System.Reflection.AssemblyTitleAttribute("snakeGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
