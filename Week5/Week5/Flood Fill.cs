3330c8af4bc77dac4450394ed85a7fd25d32d6c8
BakGuno <129590082+BakGuno@users.noreply.github.com>
2024-01-05 18:17:47 +0900

    5주차 과제2 다른 버전
    2차원배열 말고 가변배열로도 만들어봄

--- Week5/Week5/Flood Fill.cs
+++ Week5/Week5/Flood Fill.cs
@@ -12,17 +12,17 @@
 //        {
 //            Console.WriteLine("사용하실 배열의 행 크기를 입력하세요. 50이하");
 //            string inputx = Console.ReadLine();
-//            int row =int.Parse(inputx);
+//            int row = int.Parse(inputx);
 
 //            Console.WriteLine("사용하실 배열의 열 크기를 입력하세요. 50이하");
 //            string inputy = Console.ReadLine();
 //            int Col = int.Parse(inputy);
 
-//            int[,] grid = new int[row, Col]; 
-//            bool[,] visited = new bool[row,Col];
+//            int[,] grid = new int[row, Col];
+//            bool[,] visited = new bool[row, Col];
 
-//            Random rand     = new Random();
-//            for (int i=0;i<row;i++)
+//            Random rand = new Random();
+//            for (int i = 0; i < row; i++)
 //            {
 //                for (int j = 0; j < Col; j++)
 //                {
@@ -30,9 +30,9 @@
 //                    Console.Write($"[{grid[i, j]}]");
 //                }
 //                Console.WriteLine();
-//            }                
+//            }
 
-//            Console.WriteLine("시작 지점을 입력해주세요. 행 : 0~{0}, 열 :0~{1}사이입니다.",row, Col);
+//            Console.WriteLine("시작 지점을 입력해주세요. 행 : 0~{0}, 열 :0~{1}사이입니다.", row, Col);
 //            string start = Console.ReadLine();
 //            string[] starts = start.Split(",");
 //            int startx = int.Parse(starts[0]);
@@ -43,35 +43,35 @@
 //            string Changeinput = Console.ReadLine();
 //            int change = int.Parse(Changeinput);
 
-//            Find(grid, startx, starty,visited, change);
+//            Find(grid, startx, starty, visited, change);
 
 //            for (int i = 0; i < row; i++)  //완성된 배열 출력
 //            {
 //                for (int j = 0; j < Col; j++)
 //                {
-//                    Console.Write($"[{grid[i,j]}]"); 
+//                    Console.Write($"[{grid[i, j]}]");
 //                }
 //                Console.WriteLine();
 //            }
 //        }
 
-//        static int[,] Find(int[,] grid,int x, int y, bool[,] visited,int change)
-//        {           
-//                if (y  >= 1) //그냥 상하좌우 순서로 이동할 수 있는지 부터 판별
-//                    if (grid[x,y-1] == grid[x, y]) //그 후 이동할 곳의 값이 지금 있는 곳과 같고
-//                        if (visited[x,y-1] == false) //내가 들른 적이 없다면
+//        static int[,] Find(int[,] grid, int x, int y, bool[,] visited, int change)
+//        {
+//            if (y >= 1) //그냥 상하좌우 순서로 이동할 수 있는지 부터 판별
+//                if (grid[x, y - 1] == grid[x, y]) //그 후 이동할 곳의 값이 지금 있는 곳과 같고
+//                    if (visited[x, y - 1] == false) //내가 들른 적이 없다면
 //                    {
 //                        visited[x, y - 1] = true; //들른 곳으로 체크하고
-//                        Find(grid, x, y - 1, visited,change); //거기서 더 들를 곳이 있는지 확인             
+//                        Find(grid, x, y - 1, visited, change); //거기서 더 들를 곳이 있는지 확인             
 //                    }
-                            
-                    
-//                if (y < grid.GetLength(1)-1)
-//                    if (grid[x, y + 1] == grid[x, y])
-//                        if (visited[x, y + 1] == false)
+
+
+//            if (y < grid.GetLength(1) - 1)
+//                if (grid[x, y + 1] == grid[x, y])
+//                    if (visited[x, y + 1] == false)
 //                    {
 //                        visited[x, y + 1] = true;
-//                        Find(grid, x, y + 1, visited, change);                        
+//                        Find(grid, x, y + 1, visited, change);
 //                    }
 
 
@@ -79,19 +79,19 @@
 //                if (grid[x + 1, y] == grid[x, y])
 //                    if (visited[x + 1, y] == false)
 //                    {
-//                        visited[x + 1, y] = true; 
-//                        Find(grid, x + 1, y, visited, change);                        
+//                        visited[x + 1, y] = true;
+//                        Find(grid, x + 1, y, visited, change);
 //                    }
 
 
-//            if (x >=1)
-//                    if (grid[x-1, y] == grid[x, y])
-//                        if (visited[x-1, y] == false)
+//            if (x >= 1)
+//                if (grid[x - 1, y] == grid[x, y])
+//                    if (visited[x - 1, y] == false)
 //                    {
 //                        visited[x - 1, y] = true;
-//                        Find(grid, x - 1, y, visited, change);                        
+//                        Find(grid, x - 1, y, visited, change);
 //                    }
-                           
+
 //            grid[x, y] = change; //들리는게 끝나면 맨 끝 방문한 곳부터 차례차례 숫자를 바꾸면서 나옴.  
 //            return grid; //바뀐 배열 반환
 //        }
---
+++ Week5/Week5/Flood Fill2.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5
+{
+    internal class Flood_Fill2
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("사용하실 배열의 총 크기를 입력하세요. 50이하");
+            //string inputx = Console.ReadLine();
+            //int row = int.Parse(inputx);
+            Random rand = new Random();
+            int row = rand.Next(3,50); //임의의 열 크기 받기
+            int[][] arr = new int[row][];
+            bool[][] visited = new bool[row][];
+            int Col = 0;
+
+            for (int i = 0; i < row; i++) //임의의 행 크기 받아서 임의의 값 작성
+            {
+                //Console.WriteLine("사용하실 {0}번째 배열의 열 크기를 입력하세요. 50이하",i+1);
+                //string inputy = Console.ReadLine();
+                //Col = int.Parse(inputy);
+                Col = rand.Next(1,100);
+                int[] temparr = new int[Col];
+                bool[] tempbool = new bool[Col];
+                
+                for (int j = 0; j < Col; j++)
+                {
+                    temparr[j]=rand.Next(0, 2);
+                    tempbool[j] = false;
+                    Console.Write(temparr[j]);
+                }
+
+                Console.Write($"  | {i}번째 행의 크기 : {Col} ");
+                Console.WriteLine();
+
+                arr[i] = temparr;//원래는 2^16까진데 눈에 확 보이게 하려고 바꿈
+                visited[i] = tempbool; //숫자 세팅하면서 다 false로 채워주기
+            }          
+
+            Console.WriteLine("시작 지점을 입력해주세요. ", Col, row);
+            string start = Console.ReadLine();
+            string[] starts = start.Split(",");
+            int startx = int.Parse(starts[0]); ;
+            int starty = int.Parse(starts[1]);
+            visited[starty][startx] = true; //시작지점만 true로
+
+            Console.WriteLine("무슨 숫자로 바꾸고 싶은 지 입력하세요.");
+            string Changeinput = Console.ReadLine();
+            int change = int.Parse(Changeinput);
+            Console.WriteLine();
+
+
+            //검수용
+            for (int i = 0; i < row; i++)  //완성된 배열 출력
+            {
+                foreach (bool j in visited[i])
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < row; i++)  //완성된 배열 출력
+            {
+                foreach (int j in arr[i])
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Find(arr, startx, starty, visited, change);
+           
+            //결과 출력
+            for (int i = 0; i < row; i++)  
+            {
+                foreach (bool j in visited[i])
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine();
+
+            for (int i = 0; i < row; i++)  
+            {
+                foreach (int j in arr[i])
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int[][] Find(int[][] arr, int x, int y, bool[][] visited, int change) //조건이 다채로움.
+        {
+            if (y >= 1 && x < arr[y - 1].Length)
+                if (arr[y - 1].Length >= x)
+                    if (arr[y - 1][x] == arr[y][x])
+                        if (visited[y - 1][x] == false)
+                        {
+                            visited[y - 1][x] = true;
+                            Find(arr, x, y - 1, visited, change);
+                        }
+
+
+            if (y < arr.Length - 1 && x < arr[y+1].Length)
+                if (arr[y + 1][x] == arr[y][x])
+                    if (visited[y + 1][x] == false)
+                    {
+                        visited[y + 1][x] = true;
+                        Find(arr, x, y + 1, visited, change);
+                    }
+
+
+            if (x < arr[y].Length - 1)
+                if (arr[y][x + 1] == arr[y][x])
+                    if (visited[y][x+1] == false)
+                    {
+                        visited[y][x+1] = true;
+                        Find(arr, x + 1, y, visited, change);
+                    }
+
+
+            if (x >= 1)
+                if (arr[y][x - 1] == arr[y][x])
+                    if (visited[y][x - 1] == false)
+                    {
+                        visited[y][x - 1] = true;
+                        Find(arr, x - 1, y, visited, change);
+                    }
+
+            arr[y][x] = change; //들리는게 끝나면 맨 끝 방문한 곳부터 차례차례 숫자를 바꾸면서 나옴.  
+            return arr; //바뀐 배열 반환
+        }
+    }
+}
